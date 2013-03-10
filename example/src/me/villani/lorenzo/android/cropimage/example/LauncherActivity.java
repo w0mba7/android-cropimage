170fcaf71c06bc28feac804807d324e98f7b1d3a
Lorenzo Villani <lorenzo@villani.me>
2013-03-10 18:13:44 +0100

    Updated example project.

--- example/src/me/villani/lorenzo/android/cropimage/example/LauncherActivity.java
+++ example/src/me/villani/lorenzo/android/cropimage/example/LauncherActivity.java
@@ -1,15 +1,33 @@
 package me.villani.lorenzo.android.cropimage.example;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.File;
 
-public class LauncherActivity extends Activity
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.android.camera.CropImageIntentBuilder;
+
+public class LauncherActivity extends Activity implements OnClickListener
 {
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
+        final View button;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        button = findViewById(R.id.button_launch);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View view) {
+        final Uri dest = Uri.fromFile(new File(getFilesDir(), "test.jpg"));
+        final CropImageIntentBuilder builder = new CropImageIntentBuilder(100, 100, dest);
+
+        startActivity(builder.getIntent(this));
     }
 }
