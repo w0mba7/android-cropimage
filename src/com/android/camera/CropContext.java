dc5e3b1e0650200af745ea80b7d5dc24f498ffc8
Pablo Gonzalez Alonso <pabs87@gmail.com>
2013-05-15 12:25:48 +0100

    Mode the constants from Builder to CropContext

--- src/com/android/camera/CropContext.java
+++ src/com/android/camera/CropContext.java
@@ -2,6 +2,18 @@ package com.android.camera;
 
 public interface CropContext extends MonitoredContext {
 
+	public static final String EXTRA_ASPECT_X = "aspectX";
+	public static final String EXTRA_ASPECT_Y = "aspectY";
+	public static final String EXTRA_OUTPUT_X = "outputX";
+	public static final String EXTRA_OUTPUT_Y = "outputY";
+	public static final String EXTRA_BITMAP_DATA = "data";
+	public static final String EXTRA_SCALE_UP_IF_NEEDED = "scaleUpIfNeeded";
+	public static final String EXTRA_SCALE = "scale";
+	public static final String EXTRA_NO_FACE_DETECTION = "noFaceDetection";
+	public static final String EXTRA_SET_WALLPAPER = "setWallpaper";
+	public static final String EXTRA_OUTPUT_FORMAT = "outputFormat";
+	public static final String EXTRA_SOURCE = "source";
+
 	public abstract boolean isWaitingToPick();
 
 	public abstract void setWaitingToPick(boolean waiting);
