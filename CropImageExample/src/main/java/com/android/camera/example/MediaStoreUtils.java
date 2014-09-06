5efc315950fb73c507120a38dcb772c519f78558
Lorenzo Villani <lorenzo@villani.me>
2014-09-06 16:20:16 +0200

    Display cropped image in example code
    References #25

--- CropImageExample/src/main/java/com/android/camera/example/MediaStoreUtils.java
+++ CropImageExample/src/main/java/com/android/camera/example/MediaStoreUtils.java
@@ -17,23 +17,4 @@ public final class MediaStoreUtils {
 
         return Intent.createChooser(intent, "Select picture");
     }
-
-    public static String getLocalMediaPath(final Context context, final Intent intent) {
-        final Uri uri = intent.getData();
-        final Cursor cursor = context.getContentResolver().query(uri,
-                new String[]{MediaStore.MediaColumns.DATA}, null, null, null);
-
-        if (cursor == null) {
-            throw new RuntimeException(String.format("Could not resolve file name for url: {0}",
-                    uri.toString()));
-        }
-
-        cursor.moveToFirst();
-
-        final String ret = cursor.getString(0);
-
-        cursor.close();
-
-        return ret;
-    }
 }
